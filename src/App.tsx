468d158ebb32e08c344a140d87e39a54999b2b24
realhardworkingdeveloper <realhardworkingdeveloper@gmail.com>
2022-04-27 16:46:47 -0800

    writing main components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
+
+import { AppContainer } from './styles';
+import { Column } from './Column';
+import { Card } from './Card';
+import { AddNewItem } from './AddNewItem';
+
 import './App.css';
+
+// interface State {
+//   count: number;
+// }
+
+// type Action = 
+//   {
+//     type: "increment"
+//   }
+//   |
+//   {
+//     type: "decrement"
+//   }
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+      <AddNewItem
+        toggleButtonText='+ Add another list'
+        onAdd={console.log} />
+    </AppContainer>
   );
 }
 
